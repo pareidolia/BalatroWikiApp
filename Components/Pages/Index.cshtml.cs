b898f54dfdb892987305dcb25aabab49d9e97eb6
Hervé PHILIPPE <rev@ik.me>
2025-06-19 12:54:28 +0200

    Correction des modèles, mais encore soucis dans les pages...

---
+++ Components/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BalatroWikiApp.Models;
+
+namespace BalatroWikiApp.Components.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly BalatroWikiApp.Models.BalatroDBContext _context;
+
+        public IndexModel(BalatroWikiApp.Models.BalatroDBContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Card> Card { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Card = await _context.Cards.ToListAsync();
+        }
+    }
+}
