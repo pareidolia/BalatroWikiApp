5e27907665984079b831a06ddd965848403fdcb8
Epo Philippe <melusie@live.fr>
2025-06-20 09:03:42 +0200

    Ajout du champ NumberJoker pour associer un nombre à un joker

---
+++ Migrations/20250620070232_NumberJoker.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BalatroWikiApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class NumberJoker : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "numberJoker",
+                table: "jokers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "numberJoker",
+                table: "jokers");
+        }
+    }
+}
